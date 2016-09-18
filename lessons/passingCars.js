54a08e0d18348ad5aa551a5c32ca044343731b56
tjboAtVidao <thomas@vidao.com>
2016-09-17 20:53:21 -0700

    practice test

---
+++ lessons/passingCars.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect; // we are using the "expect" style of Chai
+const generator = require('../inputGenerator.js');
+
+const testValue1  = generator.makeArrayOfBinary(100);
+
+
+/**
+ * array contains only 0s, or 1s
+ * the function should return −1 if the number of pairs of passing cars exceeds 1,000,000,000
+ */
+
+
+
+
+
+console.log('test', testValue1);
